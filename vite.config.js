320af3699d5636952b6fcc0a6a4f44555b4e2c81
Mayank Jhunjhunwala <jjmayank98@gmail.com>
2024-09-03 10:32:10 +0530

    add pwa to the app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "FCT Service Engineer",
+        short_name: "FCT_ServiceEngineer",
+        description: "PWA app for service engineers",
+        theme_color: "#ffffff",
+      },
+      // temp
+      // devOptions: {
+      //   enabled: true,
+      // },
+    }),
+  ],
+});
